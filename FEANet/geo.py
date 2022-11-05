e871ca6d0ffeab1417aa10c652596e5740e8684a
longfish <thomasmeng95@gmail.com>
2022-11-05 00:27:19 -0700

    created a square mesh class

--- FEANet/geo.py
+++ FEANet/geo.py
@@ -6,30 +6,32 @@ class Geometry():
     """Define square/L-shaped geometry for a given size"""
     def __init__(self, nnode_edge = 37, l_shape = False, l_cutout_size = None):
         if l_shape is False:
-            self.geometry_idx, self.boundary_value = self.square_geometry(nnode_edge)
+            self.square_geometry(nnode_edge)
         else:
-            self.geometry_idx, self.boundary_value = self.l_shaped_geometry(nnode_edge, l_cutout_size)
+            self.l_shaped_geometry(nnode_edge, l_cutout_size)
 
     def square_geometry(self, nnode_edge):
         """Defines a square geometry. Set the boundary-values to be zero"""
 
         # Define geometry, 1.0 inner points; 0.0 elsewhre
-        geometry_idx = torch.ones(1, 1, nnode_edge ,nnode_edge )
-        geometry_idx[0, 0,  0,  :] = torch.zeros(nnode_edge)
-        geometry_idx[0, 0, -1,  :] = torch.zeros(nnode_edge)
-        geometry_idx[0, 0,  :,  0] = torch.zeros(nnode_edge)
-        geometry_idx[0, 0,  :, -1] = torch.zeros(nnode_edge)
+        self.geometry_idx = torch.ones(1, 1, nnode_edge ,nnode_edge )
+        self.geometry_idx[0, 0,  0,  :] = torch.zeros(nnode_edge)
+        self.geometry_idx[0, 0, -1,  :] = torch.zeros(nnode_edge)
+        self.geometry_idx[0, 0,  :,  0] = torch.zeros(nnode_edge)
+        self.geometry_idx[0, 0,  :, -1] = torch.zeros(nnode_edge)
 
         # Define boundary values
-        boundary_value = torch.zeros_like(geometry_idx)
+        self.boundary_value = torch.zeros_like(self.geometry_idx)
 
         # Set the boundary value to be 0
-        boundary_value[0, 0,  0, :] = 0.0
-        boundary_value[0, 0,  :,-1] = 0.0
-        boundary_value[0, 0, -1, :] = 0.0
-        boundary_value[0, 0, 1:, 0] = 0.0 
-
-        return geometry_idx, boundary_value
+        self.boundary_value[0, 0,  0, :] = 0.0
+        self.boundary_value[0, 0,  :,-1] = 0.0
+        self.boundary_value[0, 0, -1, :] = 0.0
+        self.boundary_value[0, 0, 1:, 0] = 0.0 
+    
+    def set_square_bc(self, bc):
+        '''Input bc is a 2D array, only the locations at boundaries have values'''
+        self.boundary_value[0, 0, :, :] = bc
 
 
     def l_shaped_geometry(self, nnode_edge, l_cutout_size=None):
--- FEANet/mesh.py
+++ FEANet/mesh.py
@@ -1,9 +1,9 @@
 import numpy as np
 import meshio
 
-class MeshCircInterface():
-    """ Handle the square mesh with a circular region inside, quad elements
-    0: background square; 1: circular region
+class MeshCenterInterface():
+    """ Handle the square mesh with a central region inside, quad elements
+    0: background square; 1: central region
     
     Parameters:
         size: length of the square plate
@@ -120,41 +120,33 @@ class MeshCircInterface():
         self.mesh.write(outfile)
 
 class MeshSquare():
-    """ Handle the square mesh with quad elements
-    0: background square; 1: circular region
+    """ Handle a simple square mesh with quad elements
+    0: background square
     
     Parameters:
         size: length of the square plate
         nnode_edge: number of nodes along edges of the plate
     """
 
-    def __init__(self, size = 2, prop = [1,20], nnode_edge = 65, shape = 0, outfile = None):
+    def __init__(self, size = 2, nnode_edge = 65, outfile = None):
         self.size = size
         self.nnode_edge = nnode_edge
         self.points, self.cells, self.mesh = None, None, None
         self.global_pattern_center = {} # Dictionary to store global pattern maps, center nodes
-        self.phase = np.zeros(((nnode_edge-1)*(nnode_edge-1),),dtype=int)
         self.pattern = np.zeros((nnode_edge*nnode_edge,4),dtype=int) # Pattern indices of each node, 
                                                                      # e.g., [1,0,1,0] means e1 and e3 are phase-1
-        self.a = np.array(prop, dtype=np.float32) # Coefficients of linear Laplace equation
+        self.a = np.array([1.], dtype=np.float32) # Coefficients of linear Laplace equation
         # Reference pattern dictionary
-        self.ref_pattern_dict = {0:[0,0,0,0],1:[1,1,1,1],2:[0,0,0,1],3:[0,0,1,0],
-                                 4:[1,0,0,0],5:[0,1,0,0],6:[0,0,1,1],7:[1,1,0,0],
-                                 8:[0,1,1,0],9:[1,0,0,1],10:[0,1,0,1],11:[1,0,1,0],
-                                 12:[1,1,1,0],13:[1,1,0,1],14:[0,1,1,1],15:[1,0,1,1]}
+        self.ref_pattern_dict = {0:[0,0,0,0]}
         # Reference element stiffness matrix
         self.Ke = - 1./6.*np.array([[-4.,1.,2.,1.],
                                     [1.,-4.,1.,2.],
                                     [2.,1.,-4.,1.],
                                     [1.,2.,1.,-4.]], dtype=np.float32)
         self.kernel_dict = {} # Dictionary to store pytorch kernels
-        self.generate_mesh()
-        if (shape == 0):
-            self.place_circle()
-        elif(shape == 1):
-            self.place_rect()
+        self.mesh = self.generate_mesh()
+        self.mesh.cell_data['Phase'] = np.zeros(((nnode_edge-1)*(nnode_edge-1),),dtype=int) # there is only one phase
 
-        self.identify_patterns()
         self.generate_global_pattern_map()
         self.generate_kernel()
         if outfile is not None:
@@ -167,56 +159,19 @@ class MeshSquare():
         x = np.ravel(ms_x).reshape(-1,1)
         y = np.ravel(ms_y).reshape(-1,1)
         z = np.zeros_like(x, dtype=np.float32)
-        self.points = np.concatenate((x,y,z),axis=1)
+        points = np.concatenate((x,y,z),axis=1)
         n_element = (self.nnode_edge-1)*(self.nnode_edge-1)
-        nodes = np.linspace(0,self.points.shape[0],self.points.shape[0],endpoint=False,dtype=int).reshape(self.nnode_edge,self.nnode_edge)
-        self.cells = np.zeros((n_element,4),dtype=int)
-        self.cells[:,0] = np.ravel(nodes[:self.nnode_edge-1,:self.nnode_edge-1])
-        self.cells[:,1] = np.ravel(nodes[:self.nnode_edge-1,1:])
-        self.cells[:,2] = np.ravel(nodes[1:,1:])
-        self.cells[:,3] = np.ravel(nodes[1:,:self.nnode_edge-1])
-        self.mesh = meshio.Mesh(self.points, [("quad",self.cells)])
-
-    def place_circle(self, center=[0.2, 0.2], radius=0.5):
-        for i in range(self.phase.shape[0]):
-            element_centroid = np.mean(self.points[self.cells[i]],axis=0)
-            r = (element_centroid[0]-center[0])**2+(element_centroid[1]-center[1])**2
-            if(r < radius**2):
-                self.phase[i] = 1
-        self.mesh.cell_data['Phase'] = self.phase
-
-    def place_rect(self, center=[0, 0], r=0.5):
-        for i in range(self.phase.shape[0]):
-            element_centroid = np.mean(self.points[self.cells[i]],axis=0)
-            #r = (element_centroid[0]-center[0])**2+(element_centroid[1]-center[1])**2
-            if(abs(element_centroid[0]-center[0]) < r and abs(element_centroid[1]-center[1]) < r):
-                self.phase[i] = 1
-        self.mesh.cell_data['Phase'] = self.phase
-
-    def identify_patterns(self):
-        for pid, pt in enumerate(self.points):
-            elems = np.where(self.cells == pid)[0] # Find elements centered by the node
-            if elems.shape[0] < 4:
-                continue # Ignore the boundary nodes
-            for eid in elems:
-                if self.phase[eid] == 1:
-                    el_centroid = np.mean(self.points[self.cells[eid]],axis=0)
-                    if el_centroid[0]<pt[0] and el_centroid[1]<pt[1]:
-                        self.pattern[pid,0] = 1 # Identified element-e1
-                    if el_centroid[0]>pt[0] and el_centroid[1]<pt[1]:
-                        self.pattern[pid,1] = 1 # Identified element-e2
-                    if el_centroid[0]>pt[0] and el_centroid[1]>pt[1]:
-                        self.pattern[pid,2] = 1 # Identified element-e3
-                    if el_centroid[0]<pt[0] and el_centroid[1]>pt[1]:
-                        self.pattern[pid,3] = 1 # Identified element-e4
+        nodes = np.linspace(0,points.shape[0],points.shape[0],endpoint=False,dtype=int).reshape(self.nnode_edge,self.nnode_edge)
+        cells = np.zeros((n_element,4),dtype=int)
+        cells[:,0] = np.ravel(nodes[:self.nnode_edge-1,:self.nnode_edge-1])
+        cells[:,1] = np.ravel(nodes[:self.nnode_edge-1,1:])
+        cells[:,2] = np.ravel(nodes[1:,1:])
+        cells[:,3] = np.ravel(nodes[1:,:self.nnode_edge-1])
+        return meshio.Mesh(points, [("quad",cells)])
 
     def generate_global_pattern_map(self):
         """Find nodes associated with each pattern: center nodes and surrounding nodes"""
-        for pkey in self.ref_pattern_dict:
-            self.global_pattern_center[pkey] = np.zeros((self.nnode_edge*self.nnode_edge,),dtype=int)
-            for pid in range(self.points.shape[0]):
-                if np.array_equal(self.ref_pattern_dict[pkey],self.pattern[pid]):
-                    self.global_pattern_center[pkey][pid] = 1
+        self.global_pattern_center[0] = np.ones((self.nnode_edge*self.nnode_edge,),dtype=int) # only one pattern
 
     def generate_kernel(self):
         for pkey in self.ref_pattern_dict:
@@ -234,5 +189,5 @@ class MeshSquare():
             kernel[2,2] = self.a[pattern[1]]*self.Ke[3,1] # Ke2_42
             self.kernel_dict[pkey] = kernel
 
-    def save_mesh(self,outfile = 'plate_mesh.vtk'):
+    def save_mesh(self,outfile = 'mesh_square.vtk'):
         self.mesh.write(outfile)
